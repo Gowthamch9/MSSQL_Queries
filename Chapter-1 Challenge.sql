86339e22893ceac58a9b33d5a545b53c1e8dfc27
Gowtham Venkat Eathamokkala <100267313+Gowthamch9@users.noreply.github.com>
2025-08-19 16:17:51 -0500

    Delete Chapter-1 Challenge.sql

--- Chapter-1 Challenge.sql
+++
@@ -1,7 +0,0 @@
-SELECT WorkOrderID AS 'Work Order ID',
-ScrappedQty AS 'Scrapped Quantity',
-StartDate AS 'Start Date',
-EndDate AS 'End Date'
-FROM Production.WorkOrder
-WHERE ScrappedQty > 0 AND (StartDate >= '2013-12-01' AND EndDate <= '2013-12-31')
-ORDER BY ScrappedQty DESC
