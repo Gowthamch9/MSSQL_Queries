cfeac5cdd9a06879e480db8bde8dca5b07e4f7b0
Gowtham Venkat Eathamokkala <100267313+Gowthamch9@users.noreply.github.com>
2025-08-19 16:19:29 -0500

    Delete NULL Values.sql

--- NULL Values.sql
+++
@@ -1,8 +0,0 @@
-SELECT WorkOrderID, ScrappedQty, ISNULL(ScrapReasonID, 100) AS ScrapReason
-FROM Production.WorkOrder
-;
-
-
-SELECT WorkOrderID, ScrappedQty, ScrapReasonID
-FROM Production.WorkOrder
-WHERE ScrapReasonID IS NOT NULL;
